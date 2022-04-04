8c77b436c0d4740c258b72b662bf83bbe6ece321
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-04 17:16:18 +0200

    updated

--- 1_getting_started/1_hello_world.py
+++ 1_getting_started/1_hello_world.py
@@ -1,1 +1,7 @@
-print("hello python world")
+# 'message_one' is a variable
+# 'hello world' is a value
+message_one = "hello world"
+print(message_one)
+
+message_two = "hello python crash course world"
+print(message_two)
