f7fab49be9586bda1093d41b7682c180f95af82d
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-04 17:51:41 +0200

    added notes

--- 1_getting_started/1_simple_message.py
+++ 1_getting_started/1_simple_message.py
@@ -5,3 +5,6 @@ print(message_one)
 
 message_two = "hello world, again"
 print(message_two)
+
+# notes
+    # consider variables as labels that you can assign to values
---
+++ 2_variables_and_simple_data_types/1_personal_message.py
@@ -0,0 +1,12 @@
+# '.strip()' removes left and right whitespace
+# .title() capitalises the first letter of each word
+actor_one = " monty ".strip()
+message = f"Hello {actor_one.title()}, would you like to learn some 'Python' today?"
+print(message)
+
+# notes
+    # '\t' is tab
+    # '\n' is a new line
+    # '\n\t' is a new line and tab
+    # 'rstrip()' right strips whitespace
+    # 'lstrip()' left strips whitespace
