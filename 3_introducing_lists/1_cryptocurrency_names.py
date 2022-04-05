7ef46ee70113edea666d1a963bb8f5aade69b498
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-05 18:18:04 +0200

    updated comment

--- 3_introducing_lists/1_cryptocurrency_names.py
+++ 3_introducing_lists/1_cryptocurrency_names.py
@@ -1,8 +1,9 @@
 cryptocurrencies = ["bitcoin", "ethereum", "polygon", "avalanche"]
 
 # 'first_item' gets the first item in the list
-# 'last_item' gets the last item in the list
 first_item = cryptocurrencies[0]
+
+# 'last_item' gets the last item in the list
 last_item = cryptocurrencies[-1]
 
 print(first_item)
