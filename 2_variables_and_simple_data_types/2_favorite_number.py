fadc087532aef5857d6cfa4c764490a9ef4bc0a2
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-05 16:49:26 +0200

    numbers task commit

---
+++ 2_variables_and_simple_data_types/2_favorite_number.py
@@ -0,0 +1,9 @@
+# 'FAVORITE_NUMBER' is capitalised to be treated as a constant i.e. never changed
+FAVORITE_NUMBER = 29
+message = f"my favorite number is {FAVORITE_NUMBER}"
+
+print(message)
+
+# notes
+    # '21_000_000' is how you can write long numbers to make them readable
+    # 'x, y, z = 0, 0, 0' is how you can assign multiple variables at once
