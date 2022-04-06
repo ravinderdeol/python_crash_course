d2d8858740fd98b7893c56119ccffd4fcc268938
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-06 19:14:42 +0200

    started chapter 4

---
+++ 4_working_with_lists/1_animals.py
@@ -0,0 +1,10 @@
+animals = ["lion", "tiger", "leopard"]
+
+# 'animal' is a variable getting reassigned to each animal in the list as it loops through
+for animal in animals:
+    print(f"a {animal} would make a great pet ...")
+
+print("... any of these would make a great pet")
+
+# notes
+    # you can do as many operations as you want in a for loop
