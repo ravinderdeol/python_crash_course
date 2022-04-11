24227402c61440162685c3f43996f56ccae9f89e
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-11 13:50:10 +0200

    updated script names

---
+++ 4_working_with_lists/5_script.py
@@ -0,0 +1,8 @@
+
+
+# notes
+    # an immutable list is a tuple
+    # (200, 500) define a tuple
+    # (1,) use a trailing comma to define a tuple with one item
+    # you can reassign a tuple
+    # use a tuple when you have a set of value that should not be changed
