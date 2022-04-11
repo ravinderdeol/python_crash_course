2a8cc8efbec5b6d55bd9001f861cdb2543c48f16
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-11 13:59:23 +0200

    tuples exercise complete

--- 4_working_with_lists/5_script.py
+++ 4_working_with_lists/5_script.py
@@ -1,8 +1,17 @@
+# '()' defines a tuple which is immutable
+stock_watchlist = ("GOOG", "MSFT", "AAPL")
 
+print("watching:")
+for stock in stock_watchlist:
+    print(stock)
+
+# new values get set to the tuple
+stock_watchlist = ("GOOG", "MSFT", "AMZN", "AAPL", "TSLA", "TWTR")
+
+print("\nwatching (updated):")
+for stock in stock_watchlist:
+    print(stock)
 
 # notes
-    # an immutable list is a tuple
-    # (200, 500) define a tuple
-    # (1,) use a trailing comma to define a tuple with one item
-    # you can reassign a tuple
-    # use a tuple when you have a set of value that should not be changed
+    # use a tuple when you have values you do not want to change
+    # '("GOOG")' the use of a trailing comma defines a tuple with one item
