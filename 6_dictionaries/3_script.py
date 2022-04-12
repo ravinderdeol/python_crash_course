dc6cc7d09851c7bf14cf70314521a75f09845512
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-12 17:20:37 +0200

    updated comments

--- 6_dictionaries/3_script.py
+++ 6_dictionaries/3_script.py
@@ -1,9 +1,11 @@
 crypto_description = {"bitcoin": "digital gold",
     "ethereum": "programmable contracts and money",
     "ripple": "enterprise payment settlement network",
+    "basic attention": "decentralized ad network",
     }
 
-# for loop which formats and prints items in the dictionary
+# for loop using '.items()' loops through the key-value pairs in the dictionary
+# 'key' and 'value' variable can be replaced with any other variable name
 for key, value in crypto_description.items():
     print(f"{key.title()}: {value.title()}")
 
