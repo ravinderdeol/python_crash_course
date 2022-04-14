08586d5bac485d898ef6e5c495c59e3fd2111f23
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-14 15:40:56 +0200

    basic user input

---
+++ 7_user_input_and_while_loops/1_script.py
@@ -0,0 +1,17 @@
+# 'time' module handles time-related tasks
+import time
+
+# 'int(input())' converts the user input to an integer
+number_in_group = int(input("please enter the number of people in the dinner group: "))
+
+if number_in_group >= 8:
+    print(f"let me check if we have a table for {number_in_group} available...")
+
+    # 'time.sleep()' pauses the program for a given number of seconds
+    time.sleep(4)
+    print("please wait for a table :)")
+else:
+    print("your table is ready...")
+
+# notes
+    # 'input()' defaults to a string
