7f62eb278a852ddbfd7f4c729c65e30d45f99ce6
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-04-18 16:32:56 +0200

    basic function

---
+++ 8_functions/1_script.py
@@ -0,0 +1,9 @@
+# function 'favorite_book' with a parameter of 'title'
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
+
+# function call with a argument of 'animal farm'
+favorite_book("animal farm")
+
+# notes
+    # 'parameter' and 'argument' get used interchangeably
