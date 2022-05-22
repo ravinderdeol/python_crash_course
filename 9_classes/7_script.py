8fd29df0a223c84acf94a2574e062f9fa8eedf40
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-05-22 13:19:32 +0100

    thinking more abstractly with classes

--- 9_classes/7_script.py
+++ 9_classes/7_script.py
@@ -34,5 +34,3 @@ my_ice_cream.display_flavours()
 # notes
     # use inheritance if the class you are creating is a specialised version of another class
     # when a child class inherits from a parent class it takes on all its attributes and methods
-    # to override a method from the parent class in a child class use the same name
-    # you will start thinking more abstractly about modelling data with classes as your code grows
--- 9_classes/9_script.py
+++ 9_classes/9_script.py
@@ -15,6 +15,16 @@ class User:
     def greet_user(self):
         print(f"\nHi, {self.first_name} ({self.email})")
 
+class Privileges:
+
+    # method stores the list of privileges
+    def __init__(self, privileges = ["Can Add Post", "Can Delete Post", "Can Ban User"]):
+        self.privileges = privileges
+    
+    # new method to display the list of flavours
+    def show_privileges(self):
+        print(f"Privileges: {self.privileges}")
+
 class Admin(User):
     
         # inherits attributes from the parent class using the 'super' function
@@ -22,13 +32,7 @@ class Admin(User):
 
             # initialise the attributes of the parent class
             super().__init__(first_name, last_name, age, location, email)
-
-            # new attribute to store the privileges
-            self.privileges = ["Can Add Post", "Can Delete Post", "Can Ban User"]
-
-        # new method to display the list of privileges
-        def show_privileges(self):
-            print(f"Privileges: {self.privileges}")
+            self.privileges = Privileges()
 
 # create an insance of a class
 user_1 = User("John", "Doe", "25", "London, UK", "johndoe@xyz.co.uk")
@@ -38,10 +42,10 @@ user_2 = Admin("Jane", "Doe", "45", "New York, USA", "janedoe@xyz.com")
 user_1.describe_user()
 
 user_2.describe_user()
-user_2.show_privileges()
+user_2.privileges.show_privileges()
 
 user_1.greet_user()
 user_2.greet_user()
 
 # notes
-    # to override a method from the parent class in a child class use the same name
+    # you will start thinking more abstractly about modelling data with classes as your code grows
