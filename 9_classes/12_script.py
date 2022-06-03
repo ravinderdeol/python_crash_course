468a20de5805f054c163cfb16a5a48ff6c3491c2
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-06-03 18:07:10 +0200

    dice class

---
+++ 9_classes/12_script.py
@@ -0,0 +1,18 @@
+# import the a python standard library
+from random import randint
+
+class Die:
+
+    # the class has one attribute with a default value
+    def __init__(self, sides = 6):
+        self.sides = sides
+    
+    # a method to print a random number from one to six
+    def roll_die(self):
+        print(randint(1, self.sides))
+
+# using dot notation to call a method
+Die().roll_die()
+Die().roll_die()
+Die().roll_die()
+Die().roll_die()
