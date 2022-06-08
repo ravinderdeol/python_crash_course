0e15a00ab6cb36985f21588ac68beaf05b92a680
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-06-08 17:15:11 +0200

    basic appending to txt files

---
+++ 10_files_and_exceptions/2_script.py
@@ -0,0 +1,19 @@
+filename = "guest.txt"
+
+name = input("please enter your name: ").lower()
+
+# creates or opens a file in append mode
+with open(filename, "a") as file_object:
+
+    # write to the text file with each entry on a new line
+    file_object.write(name + "\n")
+
+# notes
+    # "r" opens a file in read mode
+    # "w" opens a file in write mode
+    # "a" opens a file in append mode
+    # "r+" opens a file in read and write mode
+    # if no mode is specified python defaults to read mode
+    # opening a file in write mode will overwrite the contents of the file
+    # open a file in append mode to add to the end of the file
+    # any numerical data must be converted to a string before it can be written to a file
